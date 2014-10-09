b8deab50599cb60ee8ede4a2e3a13ae8f8969dfc
Tara Boland <bolandtara@hotmail.com>
2014-10-09 09:52:23 -0400

    in class practice

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $win = $(window);
+var $sun = $('.sun');
+var $sunSection = $('.sun-section');
+var $dipperSection = $('.dipper-section');
+var $dipper = $('.dipper');
+
+$win.on('scroll', function () {
+	var scrollPos = $win.scrollTop();
+
+	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px')
+});
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
